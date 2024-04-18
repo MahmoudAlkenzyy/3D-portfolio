b6670c1094425558bb5fa9e75b09bc7209acef9b
MahmoudAlkenzyy <105129954+MahmoudAlkenzyy@users.noreply.github.com>
2024-04-18 19:15:15 +0200

    add Contact Section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { useRef, useState } from "react";
+import emailJs from "@emailjs/browser";
+import { styles } from "../style";
+import { EarthCanvas } from "./canvas";
+import { SectionWrappper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-export default Contact
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {};
+  const handleSubmit = (e) => {};
+
+  return (
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn("left", "between", 0.2, 1)}
+        className=" flex-[.75] bg-black-100 rounded-2xl p-8"
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="my-12 flex flex-col gap-8"
+        >
+          <label htmlFor="" className="flex flex-col">
+            <span className=" text-white font-medium mb-4">Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="what's your name?"
+              className="bg-tertiary py-4 px-6 placeholder:text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label htmlFor="" className="flex flex-col">
+            <span className=" text-white font-medium mb-4">Your Email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="what's your email?"
+              className="bg-tertiary py-4 px-6 placeholder:text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label htmlFor="" className="flex flex-col">
+            <span className=" text-white font-medium mb-4">Your message</span>
+            <textarea
+              rows="7"
+              type="text"
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="what do you want to say?"
+              className="bg-tertiary py-4 px-6 placeholder:text-white rounded-lg outline-none border-none font-medium"
+            />
+          </label>
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl "
+          >
+            {loading ? "Sendding..." : "send"}
+          </button>
+        </form>
+      </motion.div>
+      <motion.div
+        variants={slideIn("right", "between", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350]"
+      >
+        <EarthCanvas />
+      </motion.div>
+    </div>
+  );
+};
+
+export default SectionWrappper(Contact, "contact");
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+
+import CanavasLoader from "../Loader";
 
 const Earth = () => {
+  const earth = useGLTF("./planet/scene.gltf");
   return (
-    <div>Earth</div>
-  )
-}
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+  );
+};
 
-export default Earth
+const EarhCanavas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demond"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Suspense fallback={<CanavasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default EarhCanavas;
